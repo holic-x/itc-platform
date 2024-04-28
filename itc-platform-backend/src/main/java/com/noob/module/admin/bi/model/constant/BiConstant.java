9ca707e6877d37b389282fb6b60fd3f3fa6f79ba
holic-x <holic-x@foxmail.com>
2024-04-28 23:26:01 +0800

    feat：bi 图表信息管理模块构建
    bi 图表信息管理模块构建：
    - 图表信息管理
    - 调用AI接口获取数据（同步、异步、任务编排）

---
+++ itc-platform-backend/src/main/java/com/noob/module/admin/bi/model/constant/BiConstant.java
@@ -0,0 +1,13 @@
+package com.noob.module.admin.bi.model.constant;
+
+/**
+ * 通用常量（存放通用常量信息，可结合业务需求自定义）
+ */
+public interface BiConstant {
+
+    /**
+     * AI modelId
+     */
+    long BI_MODEL_ID = 1780947646559985666L;
+
+}
